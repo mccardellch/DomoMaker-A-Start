9ce0302a9bb578277757970a46d2a50c19fe3d4e
harrymac <hhm7826@rit.edu>
2020-10-29 13:49:55 -0400

    completed Domo D, need to update babel version

--- server/router.js
+++ server/router.js
@@ -5,16 +5,18 @@ const mid = require('./middleware');
 
 // setup routes
 const router = (app) => {
+  app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
+  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
+
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
-  app.post('/maker',mid.requiresLogin,  controllers.Domo.make);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.make);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
